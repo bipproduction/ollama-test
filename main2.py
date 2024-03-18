8393d0e665d24c825e322fe0ce5fca8cd597e1b2
bipproduction <bip.production.js@gmail.com>
2024-03-18 22:53:28 +0800

    auto push

---
+++ main2.py
@@ -0,0 +1,23 @@
+from simple_ollama_rag import SimpleOllamaRag
+
+# Membuat instance dari SimpleOllamaRag
+so_rag = SimpleOllamaRag(
+    inference_model="phi",
+    embeddings_model="nomic-embed-text",
+    tokenizer_semantic_chunk="bert-base-uncased",
+    persist_directory="db",
+    rag_data_directory="rag_data",
+    max_tokens_embeddings=100,
+    inference_config={"stop": ["\n"]},
+)
+
+# Memuat vector store
+so_rag.load_vectorstore()
+
+# Bertanya pertanyaan
+question = 'What are not true salmon?'
+# Mendapatkan respons dari RAG
+response = so_rag.rag_chain(question)
+
+# Mencetak konten pesan dari respons
+print(response["message"]["content"])
